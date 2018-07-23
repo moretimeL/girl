54bb72e1392873bfbc618288df9154d6b26883f2
孙铁军 <tiejun.sun@happylifeplat.com>
2018-07-23 14:51:12 +0800

    注释分享api

--- src/main/resources/static/js/play.js
+++ src/main/resources/static/js/play.js
@@ -7,18 +7,18 @@ $(document).ready(function(){
         async : false,
         data:{url:url},
         success : function(data) {
-            wx.config({
-                debug: false,////生产环境需要关闭debug模式
-                appId: data.appid,//appId通过微信服务号后台查看
-                timestamp: data.timestamp,//生成签名的时间戳
-                nonceStr: data.nonceStr,//生成签名的随机字符串
-                signature: data.signature,//签名
-                jsApiList: [//需要调用的JS接口列表
-                    'checkJsApi',//判断当前客户端版本是否支持指定JS接口
-                    'onMenuShareTimeline',//分享给好友
-                    'onMenuShareAppMessage'//分享到朋友圈
-                ]
-            });
+            // wx.config({
+            //     debug: false,////生产环境需要关闭debug模式
+            //     appId: data.appid,//appId通过微信服务号后台查看
+            //     timestamp: data.timestamp,//生成签名的时间戳
+            //     nonceStr: data.nonceStr,//生成签名的随机字符串
+            //     signature: data.signature,//签名
+            //     jsApiList: [//需要调用的JS接口列表
+            //         'checkJsApi',//判断当前客户端版本是否支持指定JS接口
+            //         'onMenuShareTimeline',//分享给好友
+            //         'onMenuShareAppMessage'//分享到朋友圈
+            //     ]
+            // });
         },
         error: function(xhr, status, error) {
             //alert(status);
